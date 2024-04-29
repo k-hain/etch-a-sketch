7039cd0cb23bd9ab1949f2286bd409f139dcbbdb
k-hain <155682079+k-hain@users.noreply.github.com>
2024-04-29 22:44:08 +0200

    Add event listener to color the squares

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gridContainer = document.getElementById("gridContainer");
+const gridContainer = document.getElementById('gridContainer');
 
 let gridSize = 16;
 let gridPixelSize = 640;
@@ -18,11 +18,14 @@ function drawGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
       const newSquare = document.createElement('div');
       newSquare.classList.add('square');
-      newSquare.setAttribute('style', `
-        width: ${squareSize}px;
-        height: ${squareSize}px;
-      `)
-      newRow.appendChild(newSquare);
+      newSquare.style['width'] = `${squareSize}px`;
+      newSquare.style['height'] = `${squareSize}px`;
+
+      newSquare.addEventListener('mouseenter', (event) => {
+        newSquare.style["backgroundColor"] = 'red';
+      });
+
+      newRow.appendChild(newSquare);      
     }
 
     gridContainer.appendChild(newRow);
