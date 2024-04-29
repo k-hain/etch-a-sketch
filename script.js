d9600d44a066b16f12ac036c1cc89abdf19a387c
k-hain <155682079+k-hain@users.noreply.github.com>
2024-04-30 00:28:31 +0200

    Clean the script file for better readability

--- script.js
+++ script.js
@@ -5,54 +5,40 @@ let gridSize = 16;
 let gridPixelSize = 640;
 let squareSize = gridPixelSize / gridSize;
 
-function drawGrid(gridSize) {
-  
+drawGrid(gridSize);
+
+function drawGrid(gridSize) {  
   gridContainer.style['width'] = `${gridPixelSize}px`;
-
   for (let i = 0; i < gridSize; i++) {
-
     const newRow = document.createElement('div');
     newRow.classList.add('gridRow');
-
     for (let i = 0; i < gridSize; i++) {
       const newSquare = document.createElement('div');
       newSquare.classList.add('square');
       newSquare.style['width'] = `${squareSize}px`;
       newSquare.style['height'] = `${squareSize}px`;
-
       newSquare.addEventListener('mouseenter', (event) => {
         newSquare.style["backgroundColor"] = 'red';
       });
-
       newRow.appendChild(newSquare);      
     }
-
     gridContainer.appendChild(newRow);
-
   }
 }
 
-drawGrid(gridSize);
-
-buttons.addEventListener('click', (event) => {
-  
+buttons.addEventListener('click', (event) => {  
   let target = event.target;
-
   switch(target.id) {
     case 'resizeButton':
       while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.lastChild);
       }
-
       gridSize = 0;
       while (gridSize <= 0 || gridSize > 100 || isNaN(gridSize)) {
         gridSize = parseInt(prompt('Set grid size (1 to 100)'));
       }
-
       squareSize = gridPixelSize / gridSize;
       drawGrid(gridSize);
-
       break;
   }
-
 });
