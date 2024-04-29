a663f63c5ddcc80b2252711a3139be09ed0b265b
k-hain <155682079+k-hain@users.noreply.github.com>
2024-04-29 22:26:40 +0200

    Add ability to draw multiple rows of squares

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const gridContainer = document.getElementById("gridContainer");
 
 let gridSize = 16;
-let gridPixelSize = 1024;
+let gridPixelSize = 640;
 let squareSize = gridPixelSize / gridSize;
 
 function drawGrid(gridSize) {
+  
+  gridContainer.setAttribute('style', `
+    width: ${gridPixelSize}px;
+  `)
 
   for (let i = 0; i < gridSize; i++) {
-    const newSquare = document.createElement('div');
-    newSquare.classList.add('square');
-    newSquare.setAttribute('style', `
-      width: ${squareSize}px;
-      height: ${squareSize}px;
-    `)
-    gridContainer.appendChild(newSquare);
-  }
 
+    const newRow = document.createElement('div');
+    newRow.classList.add('gridRow');
+
+    for (let i = 0; i < gridSize; i++) {
+      const newSquare = document.createElement('div');
+      newSquare.classList.add('square');
+      newSquare.setAttribute('style', `
+        width: ${squareSize}px;
+        height: ${squareSize}px;
+      `)
+      newRow.appendChild(newSquare);
+    }
+
+    gridContainer.appendChild(newRow);
+
+  }
 }
 
 drawGrid(gridSize);
