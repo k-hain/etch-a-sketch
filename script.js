8aaf35074e26b8e7005f4e3b5a170f39dda0701c
k-hain <155682079+k-hain@users.noreply.github.com>
2024-04-30 11:51:23 +0200

    Add second drawing style
    User can choose between two drawing styles - rainbow (random RGB values
    for each mouse over) or sketch (opacity increases with each pass).

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const gridContainer = document.getElementById('gridContainer');
-const buttons = document.querySelector('#buttonContainer');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const sketchBtn = document.getElementById('sketchBtn');
 
 let gridSize = 16;
 let gridPixelSize = 640;
 let squareSize = 0;
+let drawingStyle = ''
 
 drawGrid();
+changeStyle('rainbow', true);
 
 function drawGrid() {
   squareSize = gridPixelSize / gridSize;
@@ -18,8 +21,22 @@ function drawGrid() {
       newSquare.classList.add('square');
       newSquare.style['width'] = `${squareSize}px`;
       newSquare.style['height'] = `${squareSize}px`;
+      switch(drawingStyle) {
+        case 'rainbow':
+          newSquare.style['opacity'] = 1;
+          break;
+        case 'sketch':
+          newSquare.style['opacity'] = 0;
+          newSquare.style["backgroundColor"] = 'black';
+      }
       newSquare.addEventListener('mouseenter', (event) => {
-        newSquare.style["backgroundColor"] = randomColor();
+        switch(drawingStyle) {
+          case 'rainbow':
+            newSquare.style["backgroundColor"] = randomColor();
+            break;
+          case 'sketch':
+            newSquare.style['opacity'] = +newSquare.style['opacity'] + 0.1;
+        } 
       });
       newRow.appendChild(newSquare);      
     }
@@ -52,4 +69,24 @@ function randomColor() {
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
   return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function changeStyle(newStyle, firstTime = false) {
+  drawingStyle = newStyle;
+  switch(drawingStyle) {
+    case 'rainbow':
+      sketchBtn.classList.remove('activeStyle');
+      rainbowBtn.classList.remove('disabledStyle');
+      rainbowBtn.classList.add('activeStyle');
+      sketchBtn.classList.add('disabledStyle');
+      break;
+    case 'sketch':
+      rainbowBtn.classList.remove('activeStyle');
+      sketchBtn.classList.remove('disabledStyle');
+      sketchBtn.classList.add('activeStyle');
+      rainbowBtn.classList.add('disabledStyle');
+  }
+  if (!firstTime) {
+    clearGrid();
+  }
 }
